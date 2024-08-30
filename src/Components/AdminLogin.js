dc8a3670459b904e947ecaa2ee65461bb4dd8c32
Utk-858 <151568946+Utk-858@users.noreply.github.com>
2024-08-30 11:34:45 +0530

    env update

--- src/Components/AdminLogin.js
+++ src/Components/AdminLogin.js
@@ -5,6 +5,8 @@ const Credentials = [
     { email: process.env.REACT_APP_ADMIN_EMAIL_1, password: process.env.REACT_APP_ADMIN_PASSWORD_1 },
     { email: process.env.REACT_APP_ADMIN_EMAIL_2, password: process.env.REACT_APP_ADMIN_PASSWORD_2 },
     { email: process.env.REACT_APP_ADMIN_EMAIL_3, password: process.env.REACT_APP_ADMIN_PASSWORD_3 },
+    { email: process.env.REACT_APP_ADMIN_EMAIL_4, password: process.env.REACT_APP_ADMIN_PASSWORD_4 },
+
 ];
 
 function AdminLogin() {
