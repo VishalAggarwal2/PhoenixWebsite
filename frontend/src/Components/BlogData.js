028603a613bc2b82eb94002044703f8a0f6ca82a
Utk-858 <151568946+Utk-858@users.noreply.github.com>
2024-09-13 13:32:14 +0530

    database removed

---
+++ frontend/src/Components/BlogData.js
@@ -0,0 +1,48 @@
+const path = "/images/"
+const data=[
+    { id: 5,
+        url  : path+"blog0.jpg" ,
+        date : " 25 April 2022",
+        heading :" Torque ' 22 " ,
+        subheading : "The new batch of Y-21 was introduced to the basics of Robotics with the help of Differential Drive Robot. It was followed by Track completion and Robosoccer competitions with the bots that they bulit after the informative introduction."
+      },
+    { id: 1,
+      url  : path+"blog1.jpg" ,
+      date : " 22 January 2018",
+      heading :" LFR (Line Following Robot)" ,
+      subheading : "An autonomous bot that follows black lines on white surface. It is an automated guided bot that follows visual lines embedded on the floor. The bot must be able to detect particular line and keep following it. Whenever, the robot moves out to the white surface, it starts reflecting the IR rays and making the output low. "
+    },
+    { id: 2,
+      url  : path+"blog2.jpg" ,
+      date : " 22 January 2018",
+      heading :" RC Plane " ,
+      subheading : "The task was to design and make a wireless remote controlled aircraft (using electric motors only), which has all three degrees of freedom including yaw, pitch and roll and that can complete a specified path and perform manoeuvres in the minimum possible time. "
+    },
+    { id: 3,
+       url  : path +"blog3.jpg" ,
+      date : " 20 January 2018",
+      heading :" Robowar " ,
+      subheading : " The challenge was to create a robot (manually controlled / autonomous) whose sole purpose was to push, throw, flip, drag or otherwise move your opponent out of the arena within a stipulated time. This event aimed to test the robot against another in a field of combat where brute strength and cat-like reflexes hold the key to success."
+    },
+    { id: 4,
+       url  : path +"blog4.jpg" ,
+      date : " 20 January 2018",
+      heading :" Drone" ,
+      subheading : "The task was to design a wireless remote controlled flying platform to complete an obstacle course in minimum time without crashing. The length and breadth of the drone should not exceed by 70cms. The participating teams was given max time of 7 minutes to complete the track. "
+    },
+    { id: 0,
+      url  : path+"blog5.jpg" ,
+      date : " 21 January 2018",
+      heading :" Roborace and Robosoccer" ,
+      subheading : "Roborace is designing and constructing a remote controlled (wired/wireless) robot capable of not only running fast but also is capable of fighting hurdles on the track in the least time. Robosoccer reuires to build a manually controlled bot capable of playing soccer on an arena specially designed for the robotic soccer match."
+    },
+    { id: 6,
+      url  : path +"blog6.jpg" ,
+      date : " 21 January 2018",
+      heading :"School Outreach Program " ,
+      subheading : "The members of the Phoenix club go on a school outreach program months before the Plinth. We teach emerging talents the basics of robotics. We then help them to build their own bots which they use to participate in a separate competition during Plinth meant only for the school students."
+    }
+    
+
+];
+export default data;
